1023cf643b706f1bb1130f893be9c47e5b7b9863
Bruno <bruno.guarienti@live.com>
2020-07-06 09:58:50 -0300

    setup

---
+++ src/main/java/com/guarienti/introelasticsearch/IntroElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.guarienti.introelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroElasticsearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guarienti/introelasticsearch/IntroElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guarienti.introelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroElasticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
